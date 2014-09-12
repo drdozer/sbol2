7cac09edbbf6a0e3c5e55fb9dbebdc3d0ad0faf3
Matthew Pocock <turingatemyhamster@gmail.com>
2014-09-12 11:51:08 +0100

    Adding sbt project files

---
+++ project/Build.scala
@@ -0,0 +1,94 @@
+import sbt._
+import Keys._
+
+import scala.scalajs.sbtplugin.env.nodejs.NodeJSEnv
+import scala.scalajs.sbtplugin.ScalaJSPlugin._
+
+import scala.scalajs.sbtplugin.ScalaJSPlugin.ScalaJSKeys._
+
+object Build extends sbt.Build{
+  val cross = new utest.jsrunner.JsCrossBuild(
+    organization := "uk.co.turingatemyhamster",
+
+    version := "0.1.0",
+    scalaVersion := "2.11.2",
+    name := "sbol2",
+
+    // Sonatype
+    publishArtifact in Test := false,
+    publishTo <<= version { (v: String) =>
+      Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+    },
+    libraryDependencies ++= Seq(
+      "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided"
+    ),
+    autoCompilerPlugins := true,
+
+    addCompilerPlugin("com.lihaoyi" %% "acyclic" % "0.1.2"),
+    pomExtra :=
+      <url>https://github.com/turingatemyhamster/sbol2</url>
+      <developers>
+        <developer>
+          <id>turingatemyhamster</id>
+          <name>Matthew Pocock</name>
+          <url>https://github.com/turingatemyhamster</url>
+        </developer>
+      </developers>
+
+  )
+
+  val baseSettings = Seq(
+    scalaVersion := "2.11.2",
+    organization := "uk.co.turingatemyhamster",
+    version := "0.1.0",
+    libraryDependencies ++= Seq(
+      "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided",
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value % "provided",
+      "uk.co.turingatemyhamster" %%% "datatree" % "0.1.0",
+      "com.lihaoyi" %%% "utest" % "0.2.3"
+    ),
+    addCompilerPlugin("com.lihaoyi" %% "acyclic" % "0.1.2")
+  ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings
+
+  lazy val baseJvm = project.in(file("base")).settings(baseSettings:_*).settings(
+    target := target.value / "sbol2-base-jvm",
+    moduleName := "sbol2-base"
+  )
+
+  lazy val baseJs = project.in(file("base")).settings(scalaJSSettings ++ baseSettings:_*).settings(
+    target := target.value / "sbol2-base-js",
+    moduleName := "sbol2-base"
+  )
+
+  lazy val packagesJvm = cross.jvm.
+    dependsOn(baseJvm % "compile->compile;test->test").
+    in(file("packages")).
+    settings(baseSettings:_*).
+    settings(
+    target := target.value / "sbol2-packages-jvm",
+    moduleName := "sbol2-packages",
+    scalacOptions ++= Seq("-Ymacro-debug-verbose", "Ytyper-debug")
+  )
+
+  lazy val packagesJs = cross.js.
+    dependsOn(baseJs % "compile->compile;test->test").
+    in(file("packages")).
+    settings(scalaJSSettings ++ baseSettings:_*).
+    settings(
+    target := target.value / "sbol2-packages-js",
+    moduleName := "sbol2-packages",
+      scalacOptions ++= Seq("-Ymacro-debug-lite")
+  )
+
+  lazy val jvm = cross.jvm.dependsOn(packagesJvm % "compile->compile;test->test").settings(
+    scalaVersion := "2.11.2",
+    resolvers += "bintray/non" at "http://dl.bintray.com/non/maven",
+    libraryDependencies += "org.jsawn" %% "jawn-parser" % "0.5.4"
+  ).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings :_*)
+
+  lazy val js = cross.js.dependsOn(packagesJs % "compile->compile;test->test").settings(
+    scalaVersion := "2.11.2",
+    (jsEnv in Test) := new NodeJSEnv
+  ).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings :_*)
+}
+
