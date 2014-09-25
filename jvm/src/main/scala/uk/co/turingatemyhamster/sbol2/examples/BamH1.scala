c725aaface1d9d483c2157b5055cc9edd3cdb23c
Matthew Pocock <turingatemyhamster@gmail.com>
2014-09-25 23:52:55 +0100

    Cleaned up the BamH1 example

--- jvm/src/main/scala/uk/co/turingatemyhamster/sbol2/examples/BamH1.scala
+++ jvm/src/main/scala/uk/co/turingatemyhamster/sbol2/examples/BamH1.scala
@@ -6,6 +6,7 @@ import javax.xml.stream.{XMLInputFactory, XMLOutputFactory}
 
 import com.sun.xml.internal.txw2.output.IndentingXMLStreamWriter
 import uk.co.turingatemyhamster.datatree.{RdfIo, Datatree, Datatrees}
+import uk.co.turingatemyhamster.web.Web2Web
 
 /**
  *
@@ -56,26 +57,11 @@ object BamH1 {
     println(sbolDocument)
 
 
-    implicit def prefixs(pf: SBOL2.Prefix): Datatrees.Prefix = pf match { case SBOL2.Prefix(pf) => Datatrees.Prefix(pf) }
-    implicit def sxiferp(pf: Datatrees.Prefix): SBOL2.Prefix = pf match { case Datatrees.Prefix(pf) => SBOL2.Prefix(pf) }
-    implicit def localNames(ln: SBOL2.LocalName): Datatrees.LocalName = ln match { case SBOL2.LocalName(ln) => Datatrees.LocalName(ln) }
-    implicit def semanLocol(ln: Datatrees.LocalName): SBOL2.LocalName = ln match { case Datatrees.LocalName(ln) => SBOL2.LocalName(ln) }
-    implicit def namespaces(ns: SBOL2.Namespace): Datatrees.Namespace = ns match { case SBOL2.Namespace(ns) => Datatrees.Namespace(ns) }
-    implicit def secapseman(ns: Datatrees.Namespace): SBOL2.Namespace = ns match { case Datatrees.Namespace(ns) => SBOL2.Namespace(ns) }
-    implicit def uris(uri: SBOL2.Uri): Datatrees.Uri = Datatrees.Uri(uri.raw)
-    implicit def siru(uri: Datatrees.Uri): SBOL2.Uri = SBOL2.Uri(uri.raw)
-    implicit def qnames(qname: SBOL2.QName): Datatrees.QName =
-      Datatrees.QName(
-        namespace = qname.namespace,
-        prefix = qname.prefix,
-        localName = qname.localName)
-    implicit def semanq(qname: Datatrees.QName): SBOL2.QName =
-      SBOL2.QName(
-        namespace = qname.namespace,
-        prefix = qname.prefix,
-        localName = qname.localName)
-
-    val dtree = DTIO.build(SBOL2, Datatrees)(sbolDocument)
+    val dtree = {
+      val w2w_sd = Web2Web(SBOL2, Datatrees)
+      import w2w_sd._
+      DTIO.build(SBOL2, Datatrees)(sbolDocument)
+    }
 
     println("Datatree data model")
     println(dtree)
@@ -97,7 +83,11 @@ object BamH1 {
 
     println(readDtree)
 
-    val readSbolDocument = DTIO.build(SBOL2, Datatrees)(readDtree)
+    val readSbolDocument = {
+      val w2w_ds = Web2Web(Datatrees, SBOL2)
+      import w2w_ds._
+      DTIO.build(SBOL2, Datatrees)(readDtree)
+    }
 
     println(readSbolDocument)
   }
