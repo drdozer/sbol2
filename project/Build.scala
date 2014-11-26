2ba521da77701c4d010d21f5d64fd0633f67b45e
Matthew Pocock <turingatemyhamster@gmail.com>
2014-11-26 19:48:14 +0000

    The build now works cleanly and without errors.

--- project/Build.scala
+++ project/Build.scala
@@ -10,15 +10,15 @@ object Build extends Build {
   val baseModule = XModule(id = "sbol2-base", baseDir = "base", defaultSettings = baseSettings)
 
   lazy val base                 = baseModule.project(basePlatformJvm, basePlatformJs)
-  lazy val basePlatformJvm      = baseModule.jvmProject(baseSharedJvm).settings(basePlatformSettings : _*)
-  lazy val basePlatformJs       = baseModule.jsProject(baseSharedJs).settings(basePlatformSettings : _*)
+  lazy val basePlatformJvm      = baseModule.jvmProject(baseSharedJvm).settings(basePlatformJvmSettings : _*)
+  lazy val basePlatformJs       = baseModule.jsProject(baseSharedJs).settings(basePlatformJsSettings : _*)
   lazy val baseSharedJvm        = baseModule.jvmShared().settings(baseSharedSettings : _*)
   lazy val baseSharedJs         = baseModule.jsShared(baseSharedJvm).settings(baseSharedSettings : _*)
 
   val packagesModule = XModule(id = "sbol2-packages", baseDir = "packages", defaultSettings = packagesSettings)
 
   lazy val packages             = packagesModule.project(packagesPlatformJvm, packagesPlatformJs)
-  lazy val packagesPlatformJvm  = packagesModule.jvmProject(basePlatformJvm).dependsOn(basePlatformJvm)
+  lazy val packagesPlatformJvm  = packagesModule.jvmProject(packagesSharedJvm).dependsOn(basePlatformJvm)
   lazy val packagesPlatformJs   = packagesModule.jsProject(packagesSharedJs).dependsOn(basePlatformJs)
   lazy val packagesSharedJvm    = packagesModule.jvmShared().dependsOn(baseSharedJvm)
   lazy val packagesSharedJs     = packagesModule.jsShared(packagesSharedJvm).dependsOn(baseSharedJs)
@@ -40,9 +40,15 @@ object Build extends Build {
     )
   )
 
-  def basePlatformSettings = Seq(
+  def basePlatformJvmSettings = Seq(
     libraryDependencies ++= Seq(
-      "uk.co.turingatemyhamster" %%%! "datatree" % "0.1.1"
+      "uk.co.turingatemyhamster" %% "datatree" % "0.1.1"
+    )
+  )
+
+  def basePlatformJsSettings = Seq(
+    libraryDependencies ++= Seq(
+      "uk.co.turingatemyhamster" %%% "datatree" % "0.1.1"
     )
   )
 }
