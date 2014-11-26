aab9b3a22521be7eb222913238ccc65e51f70388
Matthew Pocock <turingatemyhamster@gmail.com>
2014-11-26 15:47:13 +0000

    bumped plugin version and tweaked build dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -9,19 +9,19 @@ object Build extends Build {
 
   val baseModule = XModule(id = "sbol2-base", baseDir = "base", defaultSettings = baseSettings)
 
-  lazy val base               = baseModule.project(baseJvm, baseJs)
-  lazy val baseJvm            = baseModule.jvmProject(baseSharedJvm)
-  lazy val baseJs             = baseModule.jsProject(baseSharedJs)
-  lazy val baseSharedJvm      = baseModule.jvmShared().settings(baseSharedSettings : _*)
-  lazy val baseSharedJs       = baseModule.jsShared(baseSharedJvm).settings(baseSharedSettings : _*)
+  lazy val base                 = baseModule.project(basePlatformJvm, basePlatformJs)
+  lazy val basePlatformJvm      = baseModule.jvmProject(baseSharedJvm).settings(basePlatformSettings : _*)
+  lazy val basePlatformJs       = baseModule.jsProject(baseSharedJs).settings(basePlatformSettings : _*)
+  lazy val baseSharedJvm        = baseModule.jvmShared().settings(baseSharedSettings : _*)
+  lazy val baseSharedJs         = baseModule.jsShared(baseSharedJvm).settings(baseSharedSettings : _*)
 
   val packagesModule = XModule(id = "sbol2-packages", baseDir = "packages", defaultSettings = packagesSettings)
 
-  lazy val packages           = packagesModule.project(packagesJvm, packagesJs)
-  lazy val packagesJvm        = packagesModule.jvmProject(baseJvm).dependsOn(baseJvm)
-  lazy val packagesJs         = packagesModule.jsProject(packagesSharedJs).dependsOn(baseJs)
-  lazy val packagesSharedJvm  = packagesModule.jvmShared().dependsOn(baseSharedJvm)
-  lazy val packagesSharedJs   = packagesModule.jsShared(packagesSharedJvm).dependsOn(baseSharedJs)
+  lazy val packages             = packagesModule.project(packagesPlatformJvm, packagesPlatformJs)
+  lazy val packagesPlatformJvm  = packagesModule.jvmProject(basePlatformJvm).dependsOn(basePlatformJvm)
+  lazy val packagesPlatformJs   = packagesModule.jsProject(packagesSharedJs).dependsOn(basePlatformJs)
+  lazy val packagesSharedJvm    = packagesModule.jvmShared().dependsOn(baseSharedJvm)
+  lazy val packagesSharedJs     = packagesModule.jsShared(packagesSharedJvm).dependsOn(baseSharedJs)
 
   lazy val generalSettings = Seq(
     scalaVersion := "2.11.4",
@@ -36,6 +36,12 @@ object Build extends Build {
   lazy val baseSharedSettings = Seq(
     libraryDependencies ++= Seq(
       "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+      "uk.co.turingatemyhamster" %%%! "datatree_shared" % "0.1.1"
+    )
+  )
+
+  lazy val basePlatformSettings = Seq(
+    libraryDependencies ++= Seq(
       "uk.co.turingatemyhamster" %%%! "datatree" % "0.1.1"
     )
   )
