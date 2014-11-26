97e81709642c211fd12fd179c15e7271b36da914
Matthew Pocock <turingatemyhamster@gmail.com>
2014-11-26 13:04:34 +0000

    Moved things around to fit the new build layout.

--- project/Build.scala
+++ project/Build.scala
@@ -1,26 +1,37 @@
 import sbt._
 import Keys._
-
-import scala.scalajs.sbtplugin.env.nodejs.NodeJSEnv
+import com.inthenow.sbt.scalajs._
+import com.inthenow.sbt.scalajs.SbtScalajs._
 import scala.scalajs.sbtplugin.ScalaJSPlugin._
+import ScalaJSKeys._
 
-import scala.scalajs.sbtplugin.ScalaJSPlugin.ScalaJSKeys._
+object Build extends Build {
 
-object Build extends sbt.Build{
+  val baseModule = XModule(id = "sbol2-base", baseDir = "base", defaultSettings = baseSettings)
 
-  lazy val root = project.in(file(".")).aggregate()
+  lazy val base               = baseModule.project(baseJvm, baseJs).settings(packageBin in Compile := file(""))
+  lazy val baseJvm            = baseModule.jvmProject(baseSharedJvm)
+  lazy val baseJs             = baseModule.jsProject(baseSharedJs)
+  lazy val baseSharedJvm      = baseModule.jvmShared()
+  lazy val baseSharedJs       = baseModule.jsShared()
 
-  lazy val sharedSettings = Seq(
-    scalaVersion := "2.11.2",
+  val packagesModule = XModule(id = "sbol2-packages", baseDir = "packages", defaultSettings = packagesSettings)
+
+  lazy val packages           = packagesModule.project(packagesJvm, packagesJs).settings(packageBin in Compile := file(""))
+  lazy val packagesJvm        = packagesModule.jvmProject(packagesSharedJvm, baseJvm)
+  lazy val packagesJs         = packagesModule.jsProject(packagesSharedJs, baseJs)
+  lazy val packagesSharedJvm  = packagesModule.jvmShared(baseSharedJvm)
+  lazy val packagesSharedJs   = packagesModule.jsShared(baseSharedJs)
+
+  lazy val generalSettings = Seq(
+    scalaVersion := "2.11.4",
+    crossScalaVersions := Seq("2.11.4", "2.11.2"),
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
     organization := "uk.co.turingatemyhamster",
-    version := "0.1.0",
-    libraryDependencies ++= Seq(
-      "com.lihaoyi" %% "acyclic" % "0.1.2" % "provided",
-      "com.lihaoyi" %%% "utest" % "0.2.3"
-    ),
-    scalacOptions := Seq("-explaintypes"),
-    addCompilerPlugin("com.lihaoyi" %% "acyclic" % "0.1.2")
-  ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings
+    version := "0.1.1")
+
+  lazy val baseSettings = generalSettings
+  lazy val packagesSettings = generalSettings
 
   lazy val sharedBaseSettings = Seq(
     libraryDependencies ++= Seq(
